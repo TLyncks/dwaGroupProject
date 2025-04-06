ca5a611e8d1c51bfa2611045e180757870163738
Alessandro La Mendola <AL1092@student.aru.ac.uk>
2025-04-06 22:14:13 +0100

    merged

--- frontend/Members/UsersJS/UserDash.js
+++ frontend/Members/UsersJS/UserDash.js
@@ -1,75 +1,74 @@
 
 
 async function fetchUserProfile() {
-    try {
-      const response = await fetch('/member/dashboard', {
-        method: 'GET',
-        credentials: 'include',
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to fetch user data');
-      }
-  
-      const userData = await response.json();
-      displayUserProfile(userData);
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-      //alert('Could not load user data. Please try again.');
-       Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Could not load user data. Please try again.'
-      });
+  try {
+    const response = await fetch('/member/dashboard', {
+      method: 'GET',
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch user data');
     }
+
+    const userData = await response.json();
+    displayUserProfile(userData);
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    //alert('Could not load user data. Please try again.');
+     Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'Could not load user data. Please try again.'
+    });
   }
+}
 
 
 
-  function displayUserProfile(userData) {
-    
-    const profileElement = document.getElementById('userProfileThis'); 
-    if (profileElement) {
-      profileElement.innerHTML = `
-        <div id="userProfileImage">
-          <img src="UserDashPhotos/defaultIcon.jpg" alt="User Profile">
+function displayUserProfile(userData) {
+  
+  const profileElement = document.getElementById('userProfileThis'); 
+  if (profileElement) {
+    profileElement.innerHTML = `
+      <div id="userProfileImage">
+        <img src="UserDashPhotos/defaultIcon.jpg" alt="User Profile">
+      </div>
+      <div class="profile-info">
+        <div class="contact-info">
+          <p class="email">${userData.email}</p>
+          <p class="member-id">ID: ${userData.ID}</p>
         </div>
-        <div class="profile-info">
-          <div class="contact-info">
-            <p class="email">${userData.email}</p>
-            <p class="member-id">ID: ${userData.ID}</p>
-          </div>
-          <div class="welcome-name">
-            <span class="welcome-text">Welcome,</span>
-            <span class="user-name">${userData.fullName}</span>
-          </div>
+        <div class="welcome-name">
+          <span class="welcome-text">Welcome</span>
+          <span class="user-name">${userData.fullName}</span>
         </div>
-        <div class="profile-settings">
-          <a href= "./Settings.html" class="btn btn-primary">Settings</a>
-        </div>
-      `;
-       Swal.fire({
-        icon: 'info',
-        title: `Welcome, ${userData.fullName}!`,
-        html: `<div style="text-align: left;">
-                  <p><strong>Email:</strong> ${userData.email}</p>
-                  <p><strong>Member ID:</strong> ${userData.ID}</p>
-                  <p><strong>If this is your first time logging in, your current default password is "password" and needs to be changed.</strong></p>
-                </div>`,
-        timer: 5000,
-        timerProgressBar: true,
-        showConfirmButton: false,
-        backdrop: 'rgba(0,0,123,0.4)'
-      });
-    } else {
-      console.error("Profile element not found.");
-    }
+      </div>
+      <div class="profile-settings">
+        <a href= "./Settings.html" class="btn btn-primary">Settings</a>
+      </div>
+    `;
+     Swal.fire({
+      icon: 'info',
+      title: `Welcome, ${userData.fullName}!`,
+      html: `<div style="text-align: left;">
+                <p><strong>Email:</strong> ${userData.email}</p>
+                <p><strong>Member ID:</strong> ${userData.ID}</p>
+                <p><strong>If this is your first time logging in, your current default password is "password" and needs to be changed.</strong></p>
+              </div>`,
+      timer: 5000,
+      timerProgressBar: true,
+      showConfirmButton: false,
+      backdrop: 'rgba(0,0,123,0.4)'
+    });
+  } else {
+    console.error("Profile element not found.");
   }
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetchUserProfile();
   
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    fetchUserProfile();
-    
-  });
-  
-  
+});
+
